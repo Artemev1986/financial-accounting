3a67329e850766a76bcab1be11c3b9f1502383a4
Mikhail_A <artemev1986@yandex.ru>
2023-07-14 17:12:59 +0500

    Create new project: financial accounting

---
+++ src/test/java/com/company/financialaccounting/FinancialAccountingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.financialaccounting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialAccountingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
